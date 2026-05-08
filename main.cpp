0045b3b529ae8d7cb78d80f05cedab4e7e178d3c
elmazyounyahmedstudente-byte <elmazyouny.ahmed.studente@itispaleocapa.it>
2026-05-08 11:34:57 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,15 +3,43 @@ using namespace std;
 struct info{
 string classetoponimo;
 string descrizionetoponimo;
-int num;
+string num;
 string subalterno;
-int cap;
-int sezioneistat;
-double lat;
-double lon;
-double location;
-
+string cap;
+string sezioneistat;
+float lat;
+float lon;
+float location;
 }; 
+
+string estrazione(string x, int pos){
+	if(pos>=x.lenght()){
+		
+		
+	}
+}
+
+void caricadati(x[],int d,){
+	ifstream fileInput("testo.txt");
+    string linea;
+
+    if (fileInput.is_open()) {
+        while (fileInput>>linea){ 
+            cout << linea << '\n';
+        }
+        fileInput.close(); 
+    } else {
+        cout << "Impossibile aprire il file";
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+}
 int main(int argc, char** argv) {
 	return 0;
 }
