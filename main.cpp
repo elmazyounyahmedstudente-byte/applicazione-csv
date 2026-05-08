29dca32a2b5ea19c2bdb23c5be7542b35ea345c4
elmazyounyahmedstudente-byte <elmazyouny.ahmed.studente@itispaleocapa.it>
2026-05-08 10:32:11 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+struct info{
+string classetoponimo;
+string descrizionetoponimo;
+int num;
+string subalterno;
+int cap;
+int sezioneistat;
+double lat;
+double lon;
+double location;
+
+}; 
+int main(int argc, char** argv) {
+	return 0;
+}
